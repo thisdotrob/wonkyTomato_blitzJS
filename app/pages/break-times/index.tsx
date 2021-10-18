8d8377e385e8e69e77650c34a442f53bb4b6269f
thisdotrob <robert.stevenson@eporta.com>
2021-10-18 11:26:19 +0100

    Use Chakra-UI components on BreakTimes list page

--- app/pages/break-times/index.tsx
+++ app/pages/break-times/index.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react"
-import { Head, Link, usePaginatedQuery, useRouter, BlitzPage, Routes } from "blitz"
+import { usePaginatedQuery, useRouter, BlitzPage } from "blitz"
+import { Button, Container, Heading, HStack, Text, VStack } from "@chakra-ui/react"
 import Layout from "app/core/layouts/Layout"
+import { TopNav } from "app/core/components/TopNav"
 import getBreakTimes from "app/break-times/queries/getBreakTimes"
 
 const ITEMS_PER_PAGE = 100
@@ -18,47 +20,43 @@ export const BreakTimesList = () => {
   const goToNextPage = () => router.push({ query: { page: page + 1 } })
 
   return (
-    <div>
-      <ul>
-        {breakTimes.map((breakTime) => (
-          <li key={breakTime.id}>
-            {breakTime.createdAt.toLocaleString()}
-            {breakTime.stoppedAt ? ` - ${breakTime.stoppedAt.toLocaleTimeString()}` : null}
-          </li>
-        ))}
-      </ul>
+    <VStack>
+      {breakTimes.map((pomodoro) => (
+        <Text key={pomodoro.id}>
+          {pomodoro.createdAt.toLocaleString()}
+          {pomodoro.stoppedAt ? ` - ${pomodoro.stoppedAt.toLocaleTimeString()}` : null}
+        </Text>
+      ))}
 
-      <button disabled={page === 0} onClick={goToPreviousPage}>
-        Previous
-      </button>
-      <button disabled={!hasMore} onClick={goToNextPage}>
-        Next
-      </button>
-    </div>
+      <HStack>
+        <Button disabled={page === 0} onClick={goToPreviousPage}>
+          Previous
+        </Button>
+        <Button disabled={!hasMore} onClick={goToNextPage}>
+          Next
+        </Button>
+      </HStack>
+    </VStack>
   )
 }
 
 const BreakTimesPage: BlitzPage = () => {
   return (
-    <>
-      <Head>
-        <title>BreakTimes</title>
-      </Head>
-
-      <p>
-        <Link href={Routes.Home()}>
-          <a>Home</a>
-        </Link>
-      </p>
-
-      <Suspense fallback={<div>Loading...</div>}>
-        <BreakTimesList />
+    <Container maxW="container.lg">
+      <Suspense fallback="Loading...">
+        <VStack spacing={0}>
+          <TopNav />
+          <VStack>
+            <Heading size="md">Breaks</Heading>
+            <BreakTimesList />
+          </VStack>
+        </VStack>
       </Suspense>
-    </>
+    </Container>
   )
 }
 
 BreakTimesPage.authenticate = true
-BreakTimesPage.getLayout = (page) => <Layout>{page}</Layout>
+BreakTimesPage.getLayout = (page) => <Layout title="BreakTimes">{page}</Layout>
 
 export default BreakTimesPage
